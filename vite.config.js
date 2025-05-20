8a88c8d416114d1006d95fb875640506e00b8c2c
Timofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-05-20 08:18:15 +0300

    JS11

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';  
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
@@ -7,12 +7,14 @@ import SortCss from 'postcss-sort-media-queries';
 export default defineConfig(({ command }) => {
   return {
     define: {
+     
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
+    root: 'src', 
     build: {
       sourcemap: true,
       rollupOptions: {
+
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
@@ -22,26 +24,26 @@ export default defineConfig(({ command }) => {
           },
           entryFileNames: chunkInfo => {
             if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
+              return 'commonHelpers.js';  
             }
             return '[name].js';
           },
           assetFileNames: assetInfo => {
             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
+              return '[name].[ext]';  
             }
-            return 'assets/[name]-[hash][extname]';
+            return 'assets/[name]-[hash][extname]';  
           },
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      outDir: '../dist', 
+      emptyOutDir: true,  
     },
     plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
+      injectHTML(),  
+      FullReload(['./src/**/**.html']),  
       SortCss({
-        sort: 'mobile-first',
+        sort: 'mobile-first',  
       }),
     ],
   };
